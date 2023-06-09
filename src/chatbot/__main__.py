506b09c96711c9b9ce78bc00b9dfaaacf990c038
brock-afk <brockbrumley95@gmail.com>
2023-06-09 09:50:20 -0500

    use fine tuned model

--- src/chatbot/__main__.py
+++ src/chatbot/__main__.py
@@ -1,8 +1,14 @@
+import openai
+
 from chatbot.console.application import Application
 from chatbot.models.openai import OpenAIPrompt
 
 if __name__ == "__main__":
     prompt_api = OpenAIPrompt()
     application = Application(prompt_api=prompt_api)
-    result = application.run()
-    print(result)
+    try:
+        result = application.run()
+    except openai.error.AuthenticationError:
+        print("Please set OPENAI_API_KEY environment variable")
+    else:
+        print(result)
--- src/chatbot/models/openai.py
+++ src/chatbot/models/openai.py
@@ -5,7 +5,7 @@ from chatbot.domain.prompt import PromptAPI, Response
 
 class OpenAIPrompt(PromptAPI):
     def __init__(
-        self, engine: str | None = "text-davinci-002", temperature: int | None = 0.5
+        self, engine: str | None = "curie:ft-personal:css-rfp-2023-06-08-21-39-03", temperature: int | None = 0
     ) -> None:
         self.engine = engine
         self.temperature = temperature
@@ -16,7 +16,7 @@ class OpenAIPrompt(PromptAPI):
             engine=self.engine,
             prompt=text,
             temperature=self.temperature,
-            max_tokens=2048,
+            max_tokens=256,
         )
 
         choices = completion.choices
